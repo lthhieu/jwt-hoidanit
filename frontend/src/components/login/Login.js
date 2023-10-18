a14f948b3cdbe006816b4304bb6403a6aeab81e4
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2023-10-18 09:09:59 +0700

    crete responsive login page

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import './Login.scss'
 const Login = () => {
     return (
-        <div className='login-container'>
-            login
-            <button className='btn btn-success'>Hi world</button>
+        <div className='login-container min-vh-100 p-3'>
+            <div className='container'>
+                <div className='mt-3 mt-md-5 row px-3'>
+                    <div className='content-left d-none d-md-block col-md-6'>
+                        <div className='text-primary h1'>JWT</div>
+                        <div className='fs-5'>JSON Web Tokens are an open, industry standard RFC 7519 method for representing claims securely between two parties.</div>
+                    </div>
+                    <div className='content-right col-12 col-md-6'>
+                        <div className='bg-white login-box px-3 pt-4 shadow d-flex flex-column gap-3'>
+                            <div className='text-primary text-center h1 d-block d-md-none'>JWT</div>
+                            <input placeholder='Email address' className='mt-2 form-control' type='text' />
+                            <input placeholder='Password' className='form-control' type='password' />
+                            <button className='btn btn-primary fs-4 fw-bold'>Log In</button>
+                            <span className='forgotten-password text-center text-primary'>Forgotten password?</span>
+                            <hr />
+                            <div className='text-center'>
+                                <button className='btn btn-success fw-bold'>Create new account</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
